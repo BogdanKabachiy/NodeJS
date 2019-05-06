27e7d5e8fd2a9d1f6cebc45b3751d1ae15d5e7f6
BogdanKabachiy <kabachiy1@gmail.com>
2019-05-06 19:39:56 +0300

    Node

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+// var tools = require('./JS.js');
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(3000);
+});
